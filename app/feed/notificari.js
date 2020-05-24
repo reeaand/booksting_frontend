31140e3a1585f00c23595cf4d6ad5d05911e33b9
Andreea Ilies <andreea.ilies@gmail.com>
2020-05-24 20:08:42 +0300

    cosmetizare feed

--- app/feed/notificari.js
+++ app/feed/notificari.js
@@ -4,24 +4,28 @@ angular.module('booksting').controller('Notif', function($scope, $http) {
         $http.post('http://localhost:8080/prietenie/accept?notif=' + idd).then(function successCallback(response) {
             console.log("ok accept");
         });
+        location.reload();
     };
 
     $scope.refuzaPr = function(idd) {
         $http.post('http://localhost:8080/prietenie/refuz?notif=' + idd).then(function successCallback(response) {
             console.log("ok refuz");
         });
+        location.reload();
     };
 
     $scope.acceptaSting = function(idd) {
         $http.post('http://localhost:8080/sting/acceptat?idNotif=' + idd).then(function successCallback(response) {
             console.log("ok accept");
         });
+        location.reload();
     };
 
     $scope.refuzaSting = function(idd) {
         $http.post('http://localhost:8080/sting/refuz?idNotif=' + idd).then(function successCallback(response) {
             console.log("ok refuz");
         });
+        location.reload();
     };
 
     $http.get('http://localhost:8080/notif/notificari?username=' + localStorage.username).then(function (response) {
@@ -32,12 +36,15 @@ angular.module('booksting').controller('Notif', function($scope, $http) {
             $scope.cv.sort((a, b) => (a.key.id < b.key.id) ? 1 : -1);
             for(let i = 0; i < $scope.cv.length; i++) {
                var para = document.createElement('p');
-               para.innerText = "De la " + $scope.cv[i].value + ": " + $scope.cv[i].key.text;
+               para.id = i+'a';
+               para.className = "center";
+               para.innerText = $scope.cv[i].value + ": " + $scope.cv[i].key.text + '\n';
                document.getElementById('Notif').appendChild(para);
                if ($scope.cv[i].key.tip === "request") {
 
                     let btnDa = document.createElement("button");
                     btnDa.innerHTML = "Accept";
+                    btnDa.className = "butoane";
                     btnDa.id = $scope.cv[i].key.id;
                     if ($scope.cv[i].key.text.startsWith("Utilizatorul")) {
                         btnDa.onclick = (function() {
@@ -48,8 +55,9 @@ angular.module('booksting').controller('Notif', function($scope, $http) {
                             $scope.acceptaSting(this.id);
                         });
                     }
-                    document.getElementById('Notif').appendChild(btnDa);
+                    document.getElementById(i+'a').appendChild(btnDa);
                     let btnNu = document.createElement("button");
+                    btnNu.className = "butoane";
                     btnNu.innerHTML = "Refuz";
                     btnNu.id = $scope.cv[i].key.id + 100;
                    if ($scope.cv[i].key.text.startsWith("Utilizatorul")) {
@@ -61,7 +69,7 @@ angular.module('booksting').controller('Notif', function($scope, $http) {
                            $scope.refuzaSting(this.id-100);
                        });
                    }
-                    document.getElementById('Notif').appendChild(btnNu);
+                    document.getElementById(i+'a').appendChild(btnNu);
 
                 }
             }
